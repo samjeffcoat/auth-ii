c71ea29c05f89eaa75668eef27ef904724a70f89
samjeffcoat <samjeffcoat@gmail.com>
2019-03-07 16:46:05 -0600

    Adds users component

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Route, NavLink, withRouter } from "react-router-dom";
 import "./App.css";
 import SignIn from "./signin/SignIn";
+import users from "./users/users";
 
 class App extends Component {
   render() {
@@ -11,11 +12,14 @@ class App extends Component {
           <nav>
             <NavLink to="/login">Login</NavLink>
             &nbsp; |&nbsp;
+            <NavLink to="/users">Users</NavLink>
+            &nbsp; |&nbsp;
             <button>Logout</button>
           </nav>
         </header>
         <main>
           <Route path="/login" component={SignIn} />
+          <Route path="/users" component={users} />
         </main>
       </>
     );
